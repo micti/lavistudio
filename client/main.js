4cce3bef788115229acf9d98e0563ff875add8d9
micti <lenhatanh86@gmail.com>
2018-10-19 16:48:51 +0700

    Fix loading logo

--- client/main.js
+++ client/main.js
@@ -130,15 +130,6 @@
     this.init = function() {      
       that.initPixi();
       that.loadPixiSprites( options.pixiSprites );
-
-      /*
-      if ( options.fullScreen === true ) {
-        window.addEventListener("resize", function( event ){ 
-          scaleToWindow( renderer.view );
-        });
-        scaleToWindow( renderer.view );  
-      }
-      */
     }
  
     /// ---------------------------
@@ -155,88 +146,6 @@
     //  START 
     /// ---------------------------           
     this.init();
-
-    
-    /// ---------------------------
-    //  HELPER FUNCTIONS
-    /// ---------------------------
-    function scaleToWindow( canvas, backgroundColor ) {
-      var scaleX, scaleY, scale, center;
-    
-      //1. Scale the canvas to the correct size
-      //Figure out the scale amount on each axis
-      scaleX = window.innerWidth / canvas.offsetWidth;
-      scaleY = window.innerHeight / canvas.offsetHeight;
-    
-      //Scale the canvas based on whichever value is less: `scaleX` or `scaleY`
-      scale = Math.min(scaleX, scaleY);
-      canvas.style.transformOrigin = "0 0";
-      canvas.style.transform = "scale(" + scale + ")";
-    
-      //2. Center the canvas.
-      //Decide whether to center the canvas vertically or horizontally.
-      //Wide canvases should be centered vertically, and 
-      //square or tall canvases should be centered horizontally
-      if (canvas.offsetWidth > canvas.offsetHeight) {
-        if (canvas.offsetWidth * scale < window.innerWidth) {
-          center = "horizontally";
-        } else {
-          center = "vertically";
-        }
-      } else {
-        if (canvas.offsetHeight * scale < window.innerHeight) {
-          center = "vertically";
-        } else {
-          center = "horizontally";
-        }
-      }
-    
-      //Center horizontally (for square or tall canvases)
-      var margin;
-      if (center === "horizontally") {
-        margin = (window.innerWidth - canvas.offsetWidth * scale) / 2;
-        canvas.style.marginTop = 0 + "px";
-        canvas.style.marginBottom = 0 + "px";
-        canvas.style.marginLeft = margin + "px";
-        canvas.style.marginRight = margin + "px";
-      }
-    
-      //Center vertically (for wide canvases) 
-      if (center === "vertically") {
-        margin = (window.innerHeight - canvas.offsetHeight * scale) / 2;
-        canvas.style.marginTop = margin + "px";
-        canvas.style.marginBottom = margin + "px";
-        canvas.style.marginLeft = 0 + "px";
-        canvas.style.marginRight = 0 + "px";
-      }
-    
-      //3. Remove any padding from the canvas  and body and set the canvas
-      //display style to "block"
-      canvas.style.paddingLeft = 0 + "px";
-      canvas.style.paddingRight = 0 + "px";
-      canvas.style.paddingTop = 0 + "px";
-      canvas.style.paddingBottom = 0 + "px";
-      canvas.style.display = "block";
-    
-      //4. Set the color of the HTML body background
-      document.body.style.backgroundColor = backgroundColor;
-    
-      //Fix some quirkiness in scaling for Safari
-      var ua = navigator.userAgent.toLowerCase();
-      if (ua.indexOf("safari") != -1) {
-        if (ua.indexOf("chrome") > -1) {
-          // Chrome
-        } else {
-          // Safari
-          //canvas.style.maxHeight = "100%";
-          //canvas.style.minHeight = "100%";
-        }
-      }
-    
-      //5. Return the `scale` value. This is important, because you'll nee this value 
-      //for correct hit testing between the pointer and sprites
-      return scale;
-    } // http://bit.ly/2y1Yk2k 
   }
 })(); 
 
@@ -747,10 +656,13 @@ let app = {
       }
 
       if (percent >= 50 && percent < 75) {
+        document.getElementById('loading-letter-l').classList.add('active')
         document.getElementById('loading-letter-a').classList.add('active')
       }
 
       if (percent >= 75) {
+        document.getElementById('loading-letter-l').classList.add('active')
+        document.getElementById('loading-letter-a').classList.add('active')
         document.getElementById('loading-letter-v').classList.add('active')
       }
     })
@@ -759,11 +671,12 @@ let app = {
       document.getElementById('loading-letter-i').classList.add('active')
       setTimeout(() => {
         let loadsc = document.getElementById('load-screen')
-        new S.Merom({el: '#load', p: {x: [0, 100, '%']}, d: 700, e: 'Power4Out', cb: () => {
+        new S.Merom({el: '#load', p: {x: [0, 100, '%']}, d: 1500, e: 'Power4Out', cb: () => {
           loadsc.style.zIndex = 0;
           loadsc.style.transform = 'translateX(-100%)'
           document.getElementById('load-logo').style.display = 'none'
           document.getElementById('load-photos').style.display = 'block'
+          // document.getElementById('load').style.opacity = 1
           app.init()
         }}).play()
       }, 1000)
@@ -781,12 +694,12 @@ let app = {
 
   init: () => {
     let spriteImagesSrc = [
-      'photos/01-cover.png',
-      'photos/02-cover.png',
-      'photos/03-cover.png',
-      'photos/04-cover.png',
-      'photos/05-cover.png',
-      'photos/06-cover.png'
+      '/photos/01-cover.png',
+      '/photos/02-cover.png',
+      '/photos/03-cover.png',
+      '/photos/04-cover.png',
+      '/photos/05-cover.png',
+      '/photos/06-cover.png'
     ]
     app.slideShow = new CanvasSlideshow({
       sprites: spriteImagesSrc,
